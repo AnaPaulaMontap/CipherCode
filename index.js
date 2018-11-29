191352151d02acc751449d7b28480e65da0a380f
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2018-11-29 00:18:42 -0300

    Primera prueba de mi archivo cipher

---
+++ index.js
@@ -0,0 +1,22 @@
+/* Acá va tu código */
+    window.onload = () => {
+        // Aca crearemos nuestro DOM
+        // Obtener mi div que contendrá mi codigo js en HTML
+        const cipherData = document.getElementById("root");
+        document.getElementById("bCipher").addEventListener("click",(evento)=>{
+            evento.preventDefault();
+            let capLetter= document.getElementById("cipherText").value;
+            let displace= parseInt(document.getElementById("offset").value);
+            
+        root.innerHTML = cipher.encode(displace,capLetter)
+        });
+        const cipherDecode = document.getElementById("root2");
+        document.getElementById("dCipher").addEventListener("click",(evento)=>{
+            evento.preventDefault();
+            let capLetter= document.getElementById("cipherText").value;
+            let displace= parseInt(document.getElementById("offset").value);
+            
+        root.innerHTML = cipher.decode(displace,capLetter)
+        }); 
+
+    } 
